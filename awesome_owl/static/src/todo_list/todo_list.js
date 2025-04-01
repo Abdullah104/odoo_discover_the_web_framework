498dbb8a514d0a90a990cba0e85b2d14219325e8
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-04-01 10:59:13 +0300

    9. Adding a todo

--- awesome_owl/static/src/todo_list/todo_list.js
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -1,9 +1,26 @@
 import { Component, useState } from "@odoo/owl";
 import { TodoItem } from "./todo_item";
+import { TodoModel } from "./todo_model";
 
 export class TodoList extends Component {
   static template = "awesome_owl.todo_list";
   static components = { TodoItem };
 
   setup = () => (this.todos = useState([]));
+
+  addTodo = (event) => {
+    if (event.keyCode !== 13) return;
+
+    if (!event.target.value) return;
+
+    this.todos.push(
+      new TodoModel({
+        id: this.todos.length + 1,
+        description: event.target.value,
+        isCompleted: false,
+      })
+    );
+
+    event.target.value = "";
+  };
 }
