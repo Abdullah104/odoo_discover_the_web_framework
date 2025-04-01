2626923a69a77a710c2b8bac5032cb39e9031a1f
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-04-01 10:38:02 +0300

    update style

--- awesome_owl/static/src/todo_list/todo_list.js
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -1,18 +1,9 @@
 import { Component, useState } from "@odoo/owl";
 import { TodoItem } from "./todo_item";
-import { TodoModel } from "./todo_model";
 
 export class TodoList extends Component {
   static template = "awesome_owl.todo_list";
   static components = { TodoItem };
 
-  setup = () =>
-    (this.todos = useState([
-      new TodoModel({
-        id: 2,
-        description: "write tutorial",
-        isCompleted: true,
-      }),
-      new TodoModel({ id: 3, description: "buy milk", isCompleted: false }),
-    ]));
+  setup = () => (this.todos = useState([]));
 }
