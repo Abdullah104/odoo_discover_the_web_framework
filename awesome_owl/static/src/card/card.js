e2a4aaa164f5b85a5a3127019beee201e47e46bf
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-04-07 05:22:48 +0300

    14. Minimizing card content

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -1,4 +1,4 @@
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Card extends Component {
   static template = "awesome_owl.card";
@@ -11,4 +11,8 @@ export class Card extends Component {
       },
     },
   };
+
+  setup = () => (this.state = useState({ opened: true }));
+
+  toggleOpened = () => this.state.opened = !this.state.opened;
 }
